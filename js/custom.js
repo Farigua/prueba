baa0cdd21ce48067deee6498132d09fc467fdf92
Farigua <65664975+Farigua@users.noreply.github.com>
2020-05-27 23:03:18 -0500

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+    $(document).ready(function () {
+        $("#lightSlider").lightSlider({
+            item: 1,
+            auto: true,
+            loop: true,
+            speed: 800,
+            mode: 'fade',
+            pager: false,
+            pauseOnHover: false,
+            pause: 4000,
+            enableDrag: false,
+
+        });
+
+    });
